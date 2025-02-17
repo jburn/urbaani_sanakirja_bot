3f3378ba8a38d0571252b37940ddb62a0d644990
Juho B <jopbruun@gmail.com>
2025-02-17 19:58:12 +0200

    functionality done

--- database.py
+++ database.py
@@ -9,41 +9,43 @@ def init_db():
     con.close()
 
 def get_definitions(word):
+    word = word.lower().strip()
     con = sqlite3.connect("words.db")
     cur = con.cursor()
     cur.execute(f"SELECT * FROM word WHERE word='{word}'")
     return cur.fetchall()
 
-try:
-    init_db()
-except:
-    pass
+if __name__ == "__main__":
+    try:
+        init_db()
+    except:
+        pass
 
-con = sqlite3.connect("words.db")
-cur = con.cursor()
+    con = sqlite3.connect("words.db")
+    cur = con.cursor()
 
-with open("new_defs.json", "r", encoding="utf-8") as rfile:
-    data = json.load(rfile)
-obj = []
-id = 0
-for word in data.keys():
-    title = data[word]["title"]
-    for definition in data[word]["list"]:
-        explanation = definition["explanation"]
-        examples = "\n\n".join(definition["examples"])
-        user = definition["user"]
-        date = definition["date"]
-        likes = definition["upvotes"]
-        dislikes = definition["downvotes"]
-        labels = definition["labels"]
-        obj.append((id, word, title, explanation, examples, user, date, likes, dislikes, str(labels)))
-        id += 1
+    with open("new_defs.json", "r", encoding="utf-8") as rfile:
+        data = json.load(rfile)
+    obj = []
+    id = 0
+    for word in data.keys():
+        title = data[word]["title"]
+        for definition in data[word]["list"]:
+            explanation = definition["explanation"]
+            examples = "\n\n".join(definition["examples"])
+            user = definition["user"]
+            date = definition["date"]
+            likes = definition["upvotes"]
+            dislikes = definition["downvotes"]
+            labels = definition["labels"]
+            obj.append((id, word, title, explanation, examples, user, date, likes, dislikes, str(labels)))
+            id += 1
 
-con.executemany("INSERT INTO word VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", obj)
+    con.executemany("INSERT INTO word VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", obj)
 
-con.commit()
+    con.commit()
 
-#for row in con.execute("SELECT * FROM word WHERE word = 'flexaa'"):
-#    print(row[2], row[3])
-#    input()
-#
+    #for row in con.execute("SELECT * FROM word WHERE word = 'flexaa'"):
+    #    print(row[2], row[3])
+    #    input()
+    #
--- main.py
+++ main.py
@@ -1,31 +1,11 @@
 import database
-import telegram
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram import Update, constants, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters, CallbackContext, CallbackQueryHandler
 
 with open('./token', 'r') as tokenfile:
     TOKEN = tokenfile.read()
 
-
-async def start(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
-
-async def button(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
-    print(query)
-        
-async def word_handler(update: telegram.Update, context: ContextTypes.DEFAULT_TYPE):
-    words = database.get_definitions(update.message.text)
-    if not words:
-        await context.bot.send_message(chat_id=update.effective_chat.id, text="Sanaa ei löytynyt")
-        return
-    keyboard = [
-            [telegram.InlineKeyboardButton("Previous", callback_data="prev")],
-            [telegram.InlineKeyboardButton(f"1/{len(words)}")],
-            [telegram.InlineKeyboardButton("Next", callback_data="next")]
-        ]
-    
-    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
-    word = words[0]
+def build_reply(word: str) -> str:
     title = word[2]
     explanation = word[3]
     examples = word[4]
@@ -33,15 +13,82 @@ async def word_handler(update: telegram.Update, context: ContextTypes.DEFAULT_TY
     date = word[6]
     likes = word[7]
     dislikes = word[8]
-    labels = list(word[9])
-    out_str =\
-f"""Käyttäjältä: {user} | <i>Postattu {date}</i>\n<b>{title}</b>\n\nℹ️ <b>Selitys</b>\n{explanation}\n\n📍<b>Esimerkit</b>\n<i>{examples}</i>
-"""
-    #update.message.reply_text("hello", reply_markup=reply_markup)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=out_str, reply_markup=reply_markup) #parse_mode=telegram.constants.ParseMode.HTML,""" 
+    reply = ""
+    reply += f"Käyttäjältä: {user} | <i>Postattu {date}</i>\n"
+    reply += f"<b>{title}</b>\n\n"
+    reply += f"ℹ️ <b>Selitys</b>\n"
+    reply += f"{explanation}\n\n"
+    if examples:
+        reply += f"📍<b>Esimerkit</b>\n"
+        reply += f"<i>{examples}</i>\n\n"
+    reply += f"👍 {likes} | 👎 {dislikes}\n"
+    return reply
 
-app = ApplicationBuilder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT, word_handler))
-app.run_polling()
+def build_keyboard(definitions: list, current_index: int) -> InlineKeyboardMarkup:
+    word = definitions[current_index][1]
+    total = len(definitions)
+    prev = (current_index - 1) % total
+    next = (current_index + 1) % total
+    prev_button = InlineKeyboardButton(
+        "⬅️ Previous", callback_data=f"def:{word}:{prev}"
+    )
+    next_button = InlineKeyboardButton(
+        "Next ➡️", callback_data=f"def:{word}:{next}"
+    )
+    middle_button = InlineKeyboardButton(f"{current_index + 1}/{total}", callback_data="none")
+
+    keyboard = [[prev_button, middle_button, next_button]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Moro {update.effective_user.first_name}! Lähetä minulle jokin sana, niin yritän etsiä sille selityksen.')
+ 
+
+async def word_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    definitions = database.get_definitions(update.message.text)
+    if not definitions:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Sanaa ei löytynyt")
+        return
+    index = 0
+    out_str = build_reply(definitions[index])
+    keyboard = build_keyboard(definitions, index)
+
+    await update.message.reply_text(out_str, reply_markup=keyboard, parse_mode=constants.ParseMode.HTML) 
+
+
+async def callback_handler(update: Update, context: CallbackContext):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "none":
+        return
+    
+    try:
+        _, word, index_str = query.data.split(":")
+        index = int(index_str)
+    except ValueError:
+        await query.edit_message_text("Invalid callback data")
+        return
+    
+    definitions = database.get_definitions(word)
+    if not definitions:
+        await query.edit_message_text("Sanaa ei löytynyt")
+        return
+    
+    index %= len(definitions)
+
+    message = build_reply(definitions[index])
+    keyboard = build_keyboard(definitions, index)
+    await query.edit_message_text(message, reply_markup=keyboard, parse_mode=constants.ParseMode.HTML)
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT, word_handler))
+    app.add_handler(CallbackQueryHandler(callback_handler, pattern=r"(def:|none)"))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
