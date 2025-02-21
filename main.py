4bd6207cc3b599d6c4befa4bedc663eef520693f
Juho B <jopbruun@gmail.com>
2025-02-21 11:43:01 +0200

    fix token

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from telegram.ext import ApplicationBuilder
 from bot import get_application_handlers
 from os import getenv
+from dotenv import load_dotenv
 
 def main():
+    load_dotenv()
     TOKEN = getenv("TELEGRAM_BOT_TOKEN")
     if not TOKEN:
-        raise ValueError("Failed to open TOKEN file")
+        raise ValueError("Failed to get TOKEN")
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handlers(get_application_handlers())
